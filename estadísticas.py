09d8a6b847c91081cb1aa79b52ad413d1ae164ac
carlospuigserver <cpuiggut@myuax.com>
2022-04-28 11:03:31 +0200

    creamos dicionarios

---
+++ estadísticas.py
@@ -0,0 +1,10 @@
+Mates = list(["math percentage"])
+Lectura = list(["reading score percentage"])
+Escritura = list(["writing score percentage"])
+
+m=len(Mates)
+
+for i in range (1,m):
+    dicc = {"notas_mates":Mates, "notas_lectura":Lectura, "notas_escritura":Escritura}
+print(m)
+
