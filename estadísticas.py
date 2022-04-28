85ca483c7fb92fb2f47af90e175b87e4192e8d86
carlospuigserver <cpuiggut@myuax.com>
2022-04-28 11:29:13 +0200

    Valores min y max

--- estadísticas.py
+++ estadísticas.py
@@ -41,3 +41,50 @@ print("La mediana en Lectura es: {}".format(medLectura))
 medEscritura=(Escritura[mediana]+Escritura[mediana+1]/2)
 print("La mediana en Lectura es: {}".format(medEscritura))
 
+
+# Valor mínimos y máximos
+
+print("Valores mínimos")
+minMates=None
+for numero in Mates:
+    if(minMates is None or numero<minMates):
+        minMates=numero
+        print("La peor nota de Mates es", minMates)
+
+minLectura=None
+for numero in Lectura:
+    if(minLectura is None or numero<minLectura):
+        minLectura=numero
+        print("La peor nota de Lectura es", minLectura)
+
+
+minEscritura=None
+for numero in Escritura:
+    if(minEscritura is None or numero<minEscritura):
+        minEscritura=numero
+        print("La peor nota de Escritura es", minEscritura)
+
+
+print("Valores máximos")
+maxMates=None
+for numero in Mates:
+    if(maxMates is None or numero>maxMates):
+        maxMates=numero
+        print("La mejor nota de Mates es", maxMates)
+
+
+maxLectura=None
+for numero in Lectura:
+    if(maxLectura is None or numero>maxLectura):
+        maxLectura=numero
+        print("La mejor nota de Lectura es", maxLectura)
+
+
+maxEscritura=None
+for numero in Escritura:
+    if(maxEscritura is None or numero>maxEscritura):
+        maxEscritura=numero
+        print("La mejor nota de Escritura es", maxEscritura)
+
+
+
